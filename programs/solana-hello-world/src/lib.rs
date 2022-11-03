000da0b9e3e7d25d8c90ebc88fe825d3bf96bd45
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-11-03 16:03:53 +0530

    build and deployed solana program to the devnet

--- programs/solana-hello-world/src/lib.rs
+++ programs/solana-hello-world/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("42SzgfMNP9FjQu16wkwTZYWnRjxwjMUijDCBQyT5i7eX");
 
 #[program]
 pub mod solana_hello_world {
